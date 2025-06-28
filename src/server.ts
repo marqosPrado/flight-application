15dc163fb67bb76f916d894efa6b890715a53c97
Marcos Prado <marcospradofelix@gmail.com>
2025-06-27 23:16:34 -0300

    fix: corrige caminho da rota no index.ts and ajusta execução do seedFlight no server.ts

--- src/server.ts
+++ src/server.ts
@@ -8,11 +8,13 @@ const port: number = 3000;
 AppDataSource.initialize()
     .then((): void => {
         console.log("Database connection established");
+    })
+    .then( async () => await seedFlight())
+    .then((): void => {
         app.express.listen(port, () => {
             console.log(`Server is running on http://localhost:${port}`);
         });
     })
-    .then( async () => await seedFlight())
     .catch((error: Error) => {
         console.error("Error during Data Source initialization", error);
     })
