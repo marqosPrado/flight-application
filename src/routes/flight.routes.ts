7630df1e4bb070413ad9fa1927f95f06639a0d51
Marcos Prado <marcospradofelix@gmail.com>
2025-06-27 23:15:07 -0300

    feat: implementa rotas para 'getAllFlights'

--- src/routes/flight.routes.ts
+++ src/routes/flight.routes.ts
@@ -1,6 +1,13 @@
 import { Request, Response, Router } from "express";
+import { FlightRepository } from "../repositories/FlightRepository";
+import { FlightService } from "../service/flight/FlightService";
+import { FlightController } from "../controller/FlightController";
+
+const repository = new FlightRepository();
+const service = new FlightService(repository);
+const controller = new FlightController(service);
 
 const router: Router = Router();
-router.get("/", (req: Request, res: Response) => {})
+router.get("/", (req: Request, res: Response) => {controller.getAllFlights(req, res)});
 
 export default router;
